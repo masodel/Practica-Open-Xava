5b6353cc18d113f4207f7b8022f0a59d79c399a0
Mauro Delgado <maurods@uamv.edu.ni>
2026-06-03 16:37:54 -0600

    feat: configuracion de postgre

--- src/main/java/ni/edu/uam/invoicing/run/invoicing.java
+++ src/main/java/ni/edu/uam/invoicing/run/invoicing.java
@@ -9,7 +9,7 @@ import org.openxava.util.*;
 public class invoicing {
 
 	public static void main(String[] args) throws Exception {
-		DBServer.start("invoicing-db"); // To use your own database comment this line and configure src/main/webapp/META-INF/context.xml
+		/*DBServer.start("invoicing-db"); // To use your own database comment this line and configure src/main/webapp/META-INF/context.xml*/
 		AppServer.run("invoicing"); // Use AppServer.run("") to run in root context
 	}
 
